4103545b2260037007be86bd6c536fd851b2a08c
YSRKEN <ysr.ken@gmail.com>
2019-02-20 23:43:44 +0900

    React-Bootstrapを導入して動作を確認した (npm install react-bootstrap bootstrap)

--- hdc-app/src/App.tsx
+++ hdc-app/src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import './App.css';
 
 import logo from './logo.svg';
@@ -6,15 +7,42 @@ import logo from './logo.svg';
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+      <>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
-      </div>
+        </div>
+        <Container>
+          <Row>
+            <Col xs={12} md={6} className="mx-auto">
+              <Form>
+                <Form.Group controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                  <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text>
+                </Form.Group>
+                <Form.Group controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                <Form.Group controlId="formBasicChecbox">
+                  <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </>
     );
   }
 }
