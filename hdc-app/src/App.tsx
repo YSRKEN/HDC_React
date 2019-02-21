50069f95ba221931c3c2275138595fc3eeec2b09
YSRKEN <ysr.ken@gmail.com>
2019-02-21 17:46:20 +0900

    Chart.jsをインストールして動作を確認した (npm install react-chartjs-2 chart.js @types/chart.js)

--- hdc-app/src/App.tsx
+++ hdc-app/src/App.tsx
@@ -1,30 +1,32 @@
 import * as React from 'react';
-import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+import { Col, Container, Form, Row } from 'react-bootstrap';
+import { Scatter } from 'react-chartjs-2';
 import './App.css';
 import InputWithSlider from './InputWithSlider';
 
 /**
  * アプリケーション全体
  */
-const App: React.SFC = () => (
-  <>
-    <Container className="my-3">
-      <Row>
-        <Col xs={12} md={6} className="mx-auto">
-          <Form>
-            <Form.Group controlId="test">
-              <InputWithSlider label="最大耐久" min={1} max={200} initialValue={35}/>
-              <InputWithSlider label="艦娘装甲" min={0} max={200} initialValue={49}/>
-              <InputWithSlider label="現在耐久" min={1} max={200} initialValue={35}/>
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              送信
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
-  </>
-);
+const App: React.SFC = () => {
+  const [graphData] = React.useState({});
+  return (
+    <>
+      <Container className="my-3">
+        <Row>
+          <Col xs={12} md={6} className="mx-auto">
+            <Form>
+              <Form.Group controlId="test">
+                <InputWithSlider label="最大耐久" min={1} max={200} initialValue={35}/>
+                <InputWithSlider label="艦娘装甲" min={0} max={200} initialValue={49}/>
+                <InputWithSlider label="現在耐久" min={1} max={200} initialValue={35}/>
+              </Form.Group>
+              <Scatter data={graphData}/>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+}
 
 export default App
